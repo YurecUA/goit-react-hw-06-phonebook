eaa4bc8eafdc15c2c64259a7564e85ed584b8889
YurecUA <81627173+YurecUA@users.noreply.github.com>
2023-01-15 04:55:35 +0200

    Update contacts-types.js

--- src/redux/contacts/contacts-types.js
+++ src/redux/contacts/contacts-types.js
@@ -3,4 +3,5 @@ const DELETE_CONTACT = 'contacts/Delete_contact';
 const ADD_FILTER = 'contacts/Add_filter';
 const CLEAR_FILTER = 'contacts/Clear_filter';
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { ADD_CONTACT, DELETE_CONTACT, ADD_FILTER, CLEAR_FILTER };
